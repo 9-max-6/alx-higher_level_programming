70c1e97a07b2843cce0178c27163f6452fa53a74
9-max-6 <mutukumaxwel@gmail.com>
2024-02-05 22:58:58 +0300

    New task

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Checks if an object is an inherited instance of a class."""
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object.
+        a_class (type): The class to check against.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Else - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
