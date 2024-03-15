b47f694b6a8392a9523274548afb45fd8c46a808
9-max-6 <mutukumaxwel@gmail.com>
2024-03-15 16:30:33 +0300

    New task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    _len = len(argv) - 1
+    if _len == 0:
+        print("0 arguments")
+    elif _len == 1:
+        print("1 argument")
+    else:
+        print("{} arguments".format(_len))
+
+    for i in range(len(argv)):
+        print("{}: {}".format(i + 1, argv[i]))
+        i += 1
