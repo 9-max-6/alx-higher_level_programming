3a2a9f8c582688bf1e8fdacf491eacd52143ddc6
9-max-6 <mutukumaxwel@gmail.com>
2024-03-19 16:34:40 +0300

    New task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,15 +5,27 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./{} <a> <operator> <b>".format(sys.argv[0]))
         sys.exit(1)
-    if sys.argv[2] not in "+-*/": 
+    if sys.argv[2] not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     elif (sys.argv[2]) == "+":
-        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], calc.add(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} + {} = {}".format(sys.argv[1],
+                                    sys.argv[3],
+                                    calc.add(int(sys.argv[1]),
+                                             int(sys.argv[3]))))
     elif (sys.argv[2]) == "-":
-        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], calc.sub(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} - {} = {}".format(sys.argv[1],
+                                    sys.argv[3],
+                                    calc.sub(int(sys.argv[1]),
+                                             int(sys.argv[3]))))
     elif (sys.argv[2]) == "*":
-        print("{} * {} = {}".format(sys.argv[1], sys.argv[3], calc.mul(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} * {} = {}".format(sys.argv[1],
+                                    sys.argv[3],
+                                    calc.mul(int(sys.argv[1]),
+                                             int(sys.argv[3]))))
     else:
-        print("{} / {} = {}".format(sys.argv[1], sys.argv[3], calc.div(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} / {} = {}".format(sys.argv[1],
+                                    sys.argv[3],
+                                    calc.div(int(sys.argv[1]),
+                                             int(sys.argv[3]))))
     sys.exit(0)
