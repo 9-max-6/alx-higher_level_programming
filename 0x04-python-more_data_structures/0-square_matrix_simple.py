73af6d7dd72787db6fbd508a5f1fdc4bcb873926
9-max-6 <mutukumxawel@gmail.com>
2024-03-20 08:59:07 +0000

    Sq matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+A function to compute the square value of all integers of a matrix
+"""
+def square_matrix_simple(matrix=[]):
+	sq_matrix = []
+	for row in matrix:
+		sq_matrix.append(list(map(lambda x: x**2, row)))
+	return sq_matrix
