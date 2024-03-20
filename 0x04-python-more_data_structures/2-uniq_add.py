a7b3472d644f224df2347f53bca87a29e913b3da
9-max-6 <mutukumxawel@gmail.com>
2024-03-20 13:57:35 +0000

    New task: unique add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+A function that adds all unique integers in a list
+"""
+
+def uniq_add(my_list=[]):
+	unique_list = []
+	su_m = 0
+	for item in my_list:
+		if item not in unique_list:
+			unique_list.append(item)
+			su_m += item
+	return su_m
