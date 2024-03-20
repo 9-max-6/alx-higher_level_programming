ab5aa3c6aeb841b0e0824631029772cadd6c3996
9-max-6 <mutukumaxwel@gmail.com>
2024-03-20 19:23:08 +0300

    Fix: indentations issues

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
 """
 A function that replaces all occuraces of an element by
 another in a new list.
 """
 
+
 def search_replace(my_list, search, replace):
-	new_list = []
-	new_list = list(map(lambda item: replace if item == search else item, my_list))
-	return new_list
+    new_list = []
+    new_list = list(map(lambda item:
+                        replace if item == search else item, my_list))
+    return new_list
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,11 +4,12 @@
 A function that adds all unique integers in a list
 """
 
+
 def uniq_add(my_list=[]):
-	unique_list = []
-	su_m = 0
-	for item in my_list:
-		if item not in unique_list:
-			unique_list.append(item)
-			su_m += item
-	return su_m
+    unique_list = []
+    su_m = 0
+    for item in my_list:
+        if item not in unique_list:
+            unique_list.append(item)
+            su_m += item
+    return su_m
