c5cbb0911dcc7782072232300a75795b010a3126
9-max-6 <mutukumaxwel@gmail.com>
2024-03-21 15:59:17 +0300

    List division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            c = my_list_1[i] / my_list_2[1]
+        except TypeError:
+            print("wrong type")
+            c = 0
+            continue
+        except ZeroDivisionError:
+            print("division by zero")
+            c = 0
+            continue
+        except IndexError:
+            print("out of range")
+            c = 0
+            continue
+        finally:
+            new_list.append(c)
+    return new_list
