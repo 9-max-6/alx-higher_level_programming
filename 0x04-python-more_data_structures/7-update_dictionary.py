4628fa4471dc1bad990d2b261bec1f0ed3646121
9-max-6 <mutukumaxwel@gmail.com>
2024-03-21 09:47:38 +0300

    New task: updating dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    a function to rreplace or add key/value in a dictionary
+    """
+
+    a_dictionary[key] = value
+
