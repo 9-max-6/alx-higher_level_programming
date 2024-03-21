06177a6d23d4489f7525b6417b990535eb05949d
9-max-6 <mutukumaxwel@gmail.com>
2024-03-21 15:24:06 +0300

    Exceptions: safe printing a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,6 +10,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             num_print += 1
         except IndexError:
-            print("")
+            break
     print("")
     return num_print
