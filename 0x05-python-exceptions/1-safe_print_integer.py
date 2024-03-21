6f94ed754dd9ca3dcd8ee480252326acd77acac5
9-max-6 <mutukumaxwel@gmail.com>
2024-03-21 15:41:37 +0300

    New tasks: safe division

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    a function that prints an integer with "{:d}".format().
+    """
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError:
+        print("School is not an integer")
---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """
+    a function that divides 2 integers and prints the result.
+    """
+
+    try:
+        c = a / b
+    except (ValueError, TypeError):
+        c = None
+    finally:
+        print("{}".format(c))
