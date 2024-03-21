c143c26c9b6cc066553becd23a22e5fc8045d308
9-max-6 <mutukumaxwel@gmail.com>
2024-03-21 09:59:21 +0300

    Fix: issues with the return statement

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,8 +3,8 @@
 
 def update_dictionary(a_dictionary, key, value):
     """
-    a function to rreplace or add key/value in a dictionary
+    a function to replace or add key/value in a dictionary
     """
 
     a_dictionary[key] = value
-
+    return a_dictionary
