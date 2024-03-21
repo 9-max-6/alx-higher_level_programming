37c4bf19032dc890bcd3558d6965c7de925530ce
9-max-6 <mutukumaxwel@gmail.com>
2024-03-21 15:50:45 +0300

    New tasks: safe division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,4 +9,5 @@ def safe_print_division(a, b):
     except (ValueError, TypeError):
         c = None
     finally:
-        print("{}".format(c))
+        print("Inside result: {}".format(c))
+        return c
