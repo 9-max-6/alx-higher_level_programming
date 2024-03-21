15e48e2d4aceb8f47489bcabd42230c5bb7fd765
9-max-6 <mutukumaxwel@gmail.com>
2024-03-21 08:59:49 +0300

    Fix: intersection not union

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-A function to create a union of two sets.
+A function to create a intersection of two sets.
 """
 
 
 def common_elements(set_1, set_2):
-    return set_1.union(set_2)
+    return set_1.intersection(set_2)
