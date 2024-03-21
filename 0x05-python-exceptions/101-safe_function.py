1adebbdad8c4c23998ab4403ef19e20e97210a73
9-max-6 <mutukumaxwel@gmail.com>
2024-03-21 16:35:29 +0300

    New task

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        d = fct(*args)
+    except Exception:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        d = None
+    finally:
+        return d
