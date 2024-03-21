769686224871f8c55665601d556f60e5888f91d9
9-max-6 <mutukumaxwel@gmail.com>
2024-03-21 16:09:45 +0300

    List division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             c = 0
             continue
         except ZeroDivisionError:
-            print("division by zero")
+            print("division by 0")
             c = 0
             continue
         except IndexError:
