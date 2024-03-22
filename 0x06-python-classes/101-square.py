98acc35a85396e3d9e94fe0bf2adc9cf91f66e01
9-max-6 <mutukumaxwel@gmail.com>
2024-03-22 16:02:51 +0300

    Fix: __str__ has to return a string

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -73,14 +73,15 @@ class Square:
 
     def __str__(self):
         """
-        The method that will be invoked whenever an object of this 
+        The method that will be invoked whenever an object of this
         class is printed.
         """
         if self.size == 0:
-            print("")
+            return
         else:
             [print("") for i in range(0, self.__position[1])]
             for i in range(0, self.__size):
                 [print(" ", end="") for j in range(0, self.__position[0])]
                 [print("#", end="") for k in range(0, self.__size)]
                 print("")
+        return ("")
