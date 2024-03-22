c955014c98f43776f8be27e1b1746a6506d35b62
9-max-6 <mutukumaxwel@gmail.com>
2024-03-22 12:31:21 +0300

    New task: classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,4 +8,8 @@ class Square:
     """
     A class to define a square with one private variable size
     """
-    __size = None
+    def __init__(self, size):
+        """
+        Construct a new square
+        """
+        self.__size = size
