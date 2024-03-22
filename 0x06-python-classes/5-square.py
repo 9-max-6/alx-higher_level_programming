de631ce1d53a77904f7689148cf1d02e56cc909e
9-max-6 <mutukumaxwel@gmail.com>
2024-03-22 12:43:37 +0300

    Fix: int not iterable

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,23 +5,35 @@ Module defining a class square
 
 
 class Square:
-    __size = 3
+    """
+    A class Square with one private variable __size and  a method
+    to determine its area, a setter for the size and a getter
+    """
 
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size <= 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
+
     def area(self):
+        """
+        Determines the area of the square
+        """
         return self.__size ** 2
+
+    @property
     def size(self):
+        """
+        A getter for the size of the square
+        """
         return self.__size
+
+    @size.setter
     def size(self, value):
+        """
+        A setter for the size value of the square
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
@@ -30,8 +42,8 @@ class Square:
         if self.size == 0:
             print("")
         else:
-            for i in self.__size:
-                for i in self.__size:
+            for i in range(self.__size):
+                for i in range(self.__size):
                     print("#", end="")
                 print("")
             print("")
