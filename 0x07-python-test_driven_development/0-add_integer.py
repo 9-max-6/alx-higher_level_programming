8df7a69f822fb2dcd5da4bb32502fcc7b38ff94b
9-max-6 <mutukumaxwel@gmail.com>
2024-03-31 09:22:31 +0300

    New test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""
+Module containing the function add_integer(a, b)
+"""
+
 
 def add_integer(a, b=98):
+    """
+    Function to safely add two integers
+    """
     types = [float, int]
     if type(a) not in types:
         raise TypeError("a must be an integer")
     if type(b) not in types:
         raise TypeError("b must be an integer")
+    if a is float('inf') or b is float('inf'):
+        raise OverflowError("cannot convert float infinity to integer")
     if type(a) is types[0]:
         a = int(a)
     if type(b) is types[0]:
