3f949353dd04c590890d21844e16b07fc8747179
9-max-6 <mutukumaxwel@gmail.com>
2024-03-31 09:14:11 +0300

    New task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,11 +3,11 @@
 def add_integer(a, b=98):
     types = [float, int]
     if type(a) not in types:
-         raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) not in types:
         raise TypeError("b must be an integer")
-    if type(a) is types[1]:
+    if type(a) is types[0]:
         a = int(a)
-    if type(b) is types[1]:
+    if type(b) is types[0]:
         b = int(b)
     return (a + b)
