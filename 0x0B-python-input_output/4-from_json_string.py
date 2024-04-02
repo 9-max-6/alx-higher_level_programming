3e135632c993b9d87344c1cf7d3d58797694a873
9-max-6 <mutukumaxwel@gmail.com>
2024-04-02 16:39:15 +0300

    New task

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,7 +5,7 @@ module - from_json_string(my_str):
 import json
 
 
-def  from_json_string(my_str):
+def from_json_string(my_str):
     """
      function that returns an object (Python
      data structure) represented by a JSON string:
