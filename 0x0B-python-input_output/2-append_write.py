f401cab9e6c076651a9b6541ea8761bf62c6a214
9-max-6 <mutukumaxwel@gmail.com>
2024-04-02 16:15:06 +0300

    New task

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-module - write_file
+module - append_write()
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """
     a function that writes a string to a text file (UTF8)
     and returns the number of characters written:
     """
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         g = f.write(text)
     return g
