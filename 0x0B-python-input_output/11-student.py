9f89d2a27902a2fc1325af9716f2d4f3c0fb7446
9-max-6 <mutukumaxwel@gmail.com>
2024-04-03 14:40:25 +0300

    New task: changing object attributes

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+module - class student
+"""
+
+
+class Student:
+    """
+    Public instance attributes:
+    first_name
+    last_name
+    age
+    Instantiation with first_name, last_name and age:
+    def __init__(self, first_name, last_name, age):
+    Public method def to_json(self): that retrieves a
+    dictionary representation of a Student instance
+    (same as 8-class_to_json.py)
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and len(attrs) >= 0:
+            new_dict = {}
+            for item in attrs:
+                try:
+                    new_dict[item] = self.__dict__[item]
+                except KeyError:
+                    pass
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        if len(json) >= 0:
+            not_fake = json.keys()
+            my_attrs = self.__dict__.keys()
+
+            for item in not_fake:
+                if item in my_attrs:
+                    self.__dict__[item] = json[item]
