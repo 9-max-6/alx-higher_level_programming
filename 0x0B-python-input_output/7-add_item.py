eb91ce5ee2422651456016bc221a538755c65c0a
9-max-6 <mutukumaxwel@gmail.com>
2024-04-03 09:26:47 +0300

    Fix: added exception handling for file not found

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,11 @@ to_file = __import__('5-save_to_json_file').save_to_json_file
 from_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
-    args_list = from_file("add_item.json")
+    try:
+        args_list = from_file("add_item.json")
+    except FileNotFoundError:
+        with open("add_item.json", 'w', encoding="utf-8") as f:
+            args_list = []
     for i in range(1, len(sys.argv)):
         args_list.append(sys.argv[i])
 
