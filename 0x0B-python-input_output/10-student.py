65eaf608aca95cbf77adca17045730da6fe057d0
9-max-6 <mutukumaxwel@gmail.com>
2024-04-03 10:12:05 +0300

    New task

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+module - class student
+"""
+
+
+class Student:
+    """
+    Public instance attributes:
+    first_name
+    last_name
+    age
+    Instantiation with first_name, last_name and age:
+    def __init__(self, first_name, last_name, age):
+    Public method def to_json(self): that retrieves a
+    dictionary representation of a Student instance
+    (same as 8-class_to_json.py)
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and len(attrs) > 0:
+            new_dict = {}
+            for item in attrs:
+                try:
+                    new_dict[item] = self.__dict__[item]
+                except KeyError:
+                    pass
+            if len(new_dict) > 0:
+                return new_dict
+            else:
+                return self.__dict__
+        else:
+            return self.__dict__
