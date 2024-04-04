b2042997073262a68fcc23a293779a54de0d8083
9-max-6 <mutukumaxwel@gmail.com>
2024-04-04 10:41:20 +0300

    New task

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module - the square class
+"""
+from models import rectangle
+
+
+class Square(rectangle.Rectangle):
+    """
+    Class square extends rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """print rectangle"""
+        args = [self.id, self.x, self.y, self.height]
+        return "[Square] ({}) {}/{} - {}".format(*args)
