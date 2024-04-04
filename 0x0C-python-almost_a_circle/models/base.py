4d8272aa4866a353509793c4416c411f35b541fb
9-max-6 <mutukumaxwel@gmail.com>
2024-04-04 11:47:04 +0300

    New task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Module - class Base
 """
+import json
 
 
 class Base:
@@ -16,3 +17,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries:"""
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
