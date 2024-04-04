d63d66ca98028b1e02c4470c3f510695e0d2b04d
9-max-6 <mutukumaxwel@gmail.com>
2024-04-04 10:22:20 +0300

    New task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,3 +95,16 @@ class Rectangle(base.Base):
         """print rectangle"""
         args = [self.id, self.x, self.y, self.width, self.height]
         return "[Rectangle] ({}) {}/{} - {}/{}".format(*args)
+
+    def update(self, *args):
+        for i in range(len(args)):
+            if (i == 0):
+                self.id = args[0]
+            if (i == 1):
+                self.width = args[1]
+            if (i == 2):
+                self.height = args[2]
+            if (i == 3):
+                self.width = args[3]
+            if (i == 4):
+                self.width = args[4]
