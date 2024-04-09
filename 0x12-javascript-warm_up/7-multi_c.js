3df4cd09ff68e1e7ce5c47f8090ec08c6ea4bc56
9-max-6 <mutukumaxwel@gmail.com>
2024-04-09 08:44:46 +0300

    Fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,9 +6,9 @@ const convertedNumber = Number(argv[2]);
 const myText = 'C is fun';
 
 if (Number.isNaN(convertedNumber)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < convertedNumber; i++) {
-		console.log(myText);
-	}
+  for (let i = 0; i < convertedNumber; i++) {
+    console.log(myText);
+  }
 }
