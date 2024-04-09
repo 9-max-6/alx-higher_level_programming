191ea05027156145daf3a2f13b51fe3101394f6c
9-max-6 <mutukumaxwel@gmail.com>
2024-04-09 08:48:42 +0300

    Fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,8 +7,8 @@ const convertedNumber = Number(argv[2]);
 if (Number.isNaN(convertedNumber)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < convertedNumber; i++) {
-    for (let i = 0; i < convertedNumber; i++) {
+  for (let i = 1; i < convertedNumber; i++) {
+    for (let i = 1; i < convertedNumber; i++) {
       process.stdout.write('X');
     }
     console.log('X');
