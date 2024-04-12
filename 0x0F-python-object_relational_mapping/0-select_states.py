be62431f5ef579872cd54fe6d5c432437683da3b
9-max-6 <mutukumaxwel@gmail.com>
2024-04-12 11:02:01 +0300

    New task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module - Connects to MySQL database using MySQLdb and retrieves
+the first entry from the 'states' table.
+"""
+import MySQLdb
+import sys
+
+try: 
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+except IndexError:
+    sys.exit(1)
+
+DB_CONFIG = {
+    "user": f"{username}",
+    "db": f"{database}",
+    "password": f"{password}",
+    "port": 3306,
+}
+
+try:
+    db = MySQLdb.connect(**DB_CONFIG)
+    cursor = db.cursor()
+    
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    
+    result = cursor.fetchall()
+    for item in result:
+        print(item)
+    
+except MySQLdb.Error as e:
+    print(f"Error connecting to MySQL: {e}")
+    
+finally:
+    try:
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
+    except NameError:
+        pass
