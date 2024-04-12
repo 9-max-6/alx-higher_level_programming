42c8e52772ce9f483545239c8903c093b48028d6
9-max-6 <mutukumaxwel@gmail.com>
2024-04-12 11:03:44 +0300

    Fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,39 +5,41 @@ the first entry from the 'states' table.
 """
 import MySQLdb
 import sys
+if __name__ == '__main__':
+    try: 
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+    except IndexError:
+        sys.exit(1)
 
-try: 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-except IndexError:
-    sys.exit(1)
+    DB_CONFIG = {
+        "user": f"{username}",
+        "db": f"{database}",
+        "password": f"{password}",
+        "port": 3306,
+    }
 
-DB_CONFIG = {
-    "user": f"{username}",
-    "db": f"{database}",
-    "password": f"{password}",
-    "port": 3306,
-}
-
-try:
-    db = MySQLdb.connect(**DB_CONFIG)
-    cursor = db.cursor()
-    
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-    
-    result = cursor.fetchall()
-    for item in result:
-        print(item)
-    
-except MySQLdb.Error as e:
-    print(f"Error connecting to MySQL: {e}")
-    
-finally:
     try:
-        if cursor:
-            cursor.close()
-        if db:
-            db.close()
-    except NameError:
-        pass
+        db = MySQLdb.connect(**DB_CONFIG)
+        cursor = db.cursor()
+        
+        cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+        
+        result = cursor.fetchall()
+        for item in result:
+            print(item)
+        
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        
+    finally:
+        try:
+            if cursor:
+                cursor.close()
+            if db:
+                db.close()
+        except NameError:
+            pass
+
+
