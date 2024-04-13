b1003f40d8e67579f860180fa45eb9147e14e7c2
9-max-6 <mutukumaxwel@gmail.com>
2024-04-13 12:44:08 +0300

    New task

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,6 @@ Base = declarative_base()
 class State(Base):
     """class state that inherits from Base"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement='auto',
+    id = Column(Integer, primary_key=True, autoincrement=True,
                 nullable=False)
     name = Column(String(128))
