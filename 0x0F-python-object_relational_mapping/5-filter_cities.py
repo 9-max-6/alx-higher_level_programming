10153698dfef5280501eef26d649897e7403eb85
9-max-6 <mutukumaxwel@gmail.com>
2024-04-13 09:49:22 +0300

    New task

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Module - Connects to MySQL database using MySQLdb and retrieves
+the first entry from the 'states' table.
+"""
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    try:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        state_name = sys.argv[4]
+    except IndexError:
+        sys.exit(1)
+
+    DB_CONFIG = {
+        "user": f"{username}",
+        "db": f"{database}",
+        "password": f"{password}",
+        "port": 3306,
+    }
+
+    try:
+        db = MySQLdb.connect(**DB_CONFIG)
+        cursor = db.cursor()
+        query = "SELECT cities.name \
+            FROM states \
+                JOIN cities \
+                    ON cities.state_id = states.id \
+                        WHERE states.name = %s;"
+        cursor.execute(query, (state_name, ))
+
+        results = cursor.fetchall()
+        for result in results:
+            print(result[0], end=" ")
+        print("")
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL: {e}")
+
+    finally:
+        try:
+            if cursor:
+                cursor.close()
+            if db:
+                db.close()
+        except NameError:
+            pass
