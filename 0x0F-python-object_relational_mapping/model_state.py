0cdb64f016d5c24532e173eac8ffda8690edc401
9-max-6 <mutukumaxwel@gmail.com>
2024-04-13 11:31:54 +0300

    New task

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A module containing the class definition of a state and and instance
+of Base
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """class state that inherits from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement='auto',
+                nullable=False)
+    name = Column(String(128))
