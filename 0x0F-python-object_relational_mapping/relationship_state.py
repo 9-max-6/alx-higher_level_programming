b738e7edbab276c32fe6de1f5a05d7eaf2ac6f63
9-max-6 <mutukumaxwel@gmail.com>
2024-04-14 09:56:31 +0300

    New task

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -15,4 +15,4 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True,
                 nullable=False)
     name = Column(String(128))
-    cities = relationship("City", backref="state", cascade="all")
+    cities = relationship("City", backref="state")
