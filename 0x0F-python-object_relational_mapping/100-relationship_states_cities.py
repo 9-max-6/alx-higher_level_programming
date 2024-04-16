dccf742d2f3dcf9ec889974f5da5e2d8a1e591e5
9-max-6 <mutukumaxwel@gmail.com>
2024-04-16 09:15:00 +0300

    New task

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,11 +12,11 @@ if __name__ == '__main__':
                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session()
 
-    newState = State(name='California')
-    newCity = City(name='San Francisco')
-    newState.cities.append(newCity)
-    session.add(newState)
-    session.commit()
-    session.close()
+    with Session() as session:
+        newState = State(name='California')
+        newCity = City(name='San Francisco')
+        newState.cities.append(newCity)
+        session.add(newState)
+        session.commit()
+        session.close()
