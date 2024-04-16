2d4903ddc013112d9e8215d7f08ec930918b91a1
9-max-6 <mutukumaxwel@gmail.com>
2024-04-16 09:10:15 +0300

    New task

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -13,3 +13,4 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128))
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship('State', back_populates='cities')
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -15,4 +15,4 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True,
                 nullable=False)
     name = Column(String(128))
-    cities = relationship("City", backref="state", cascade="delete, delete-orphan")
+    cities = relationship("City", back_populates="state", cascade="delete, delete-orphan")
