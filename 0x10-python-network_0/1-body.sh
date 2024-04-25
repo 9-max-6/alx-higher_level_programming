9db74911d94f0f94edefe1d9974b181f6ffbf0de
9-max-6 <mutukumaxwel@gmail.com>
2024-04-25 16:34:33 +0300

    Fix: changed variable

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET req, disp body
-curl -s "$1" | grep "HTTP" | grep "200" >/dev/null && curl -s 0.0.0.0:5000
+curl -s "$1" | grep "HTTP" | grep "200" > /dev/null && curl -s "$1"
