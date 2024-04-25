7f80d48a170c2b4adf108adef2190d085f5de90e
9-max-6 <mutukumaxwel@gmail.com>
2024-04-25 21:24:03 +0300

    New task

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sends a POST req to the passed URL, and disp the body of the 
+curl -X POST "$com/route_6" -d "email=test@gmail.com&subject=I%20will%20always%20be%20there%20for%20you"
