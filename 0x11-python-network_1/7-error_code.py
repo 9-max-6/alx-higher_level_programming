b48fe8c7e9d1cf51402a528874c9e0982aaf7983
9-max-6 <mutukumaxwel@gmail.com>
2024-04-26 14:54:07 +0300

    Fix: resp.raise_for_status()

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,15 +5,15 @@ displays the body of the response.
 """
 import sys
 import requests
-from requests.exceptions import HTTPError
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        url = sys.argv[0]
+        url = sys.argv[1]
         try:
             with requests.get(url) as resp:
+                resp.raise_for_status()
                 print(resp.text)
-        except HTTPError as errh:
+        except requests.exceptions.HTTPError as errh:
             if errh.response.status_code > 400:
                 print("Error code: {}".format(errh.response.status_code))
