36e7421c4a203544f49337febf14e241f5170dd4
9-max-6 <mutukumaxwel@gmail.com>
2024-04-26 14:21:35 +0300

    . Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+A script to take in a URL, send a request and disaplay
+the value ofthe variable X-Request-Id in response header.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+
+        with requests.get(url) as resp:
+            req_id = resp.headers.get("X-Request-Id")
+            print(req_id)
