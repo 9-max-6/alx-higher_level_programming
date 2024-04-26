2c1def2f7fb7caa7ff230708e6ef51505ca415d7
9-max-6 <mutukumaxwel@gmail.com>
2024-04-26 12:42:48 +0300

    What's my status? #1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+that takes in a URL, sends a request to the URL and displays th
+body of the response (decoded in utf-8).
+"""
+import sys
+import urllib.request
+import urllib.parse
+from urllib.error import HTTPError, URLError
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+
+    try:
+        resp = urllib.request.urlopen(url)
+        content_body = resp.read()
+        print(content_body.decode("utf-8"))
+
+    except HTTPError as e:
+        print("Error code: {}".format(e.__dict__.get("code")))
+    except URLError as e:
+        print("Error code: {}".format(e.__dict__.get("code")))
