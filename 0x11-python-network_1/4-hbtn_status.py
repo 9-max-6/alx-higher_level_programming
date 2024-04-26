b2620c5ff85f3783c1066a3e5a014f731abebf55
9-max-6 <mutukumaxwel@gmail.com>
2024-04-26 14:16:41 +0300

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/bin/python3
+"""A script that fetches a URL using requests package"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with requests.get(url) as resp:
+        print("Body response:")
+        print(f"    - type: {type(resp.text)}")
+        print(f"    - content: {resp.text}")
