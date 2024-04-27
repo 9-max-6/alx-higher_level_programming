043505b1be2a9b5678cb90af1747893c078a2b6a
9-max-6 <mutukumaxwel@gmail.com>
2024-04-27 07:56:53 +0300

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+ takes in a letter and sends a POST request to
+ http://0.0.0.0:5000/search_user
+ with the letter as a parameter.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+
+        pay_load = {"q": q}
+        try:
+            with requests.post(url, data=pay_load) as resp:
+                resp.raise_for_status()
+                req_id = resp.json().get("id", None)
+                name = resp.json().get("name", None)
+                if name and req_id:
+                    print(f"[{req_id}] {name}")
+                else:
+                    print("No result")
+        except requests.exceptions.Exception as err:
+            print("Not a valid JSON")
