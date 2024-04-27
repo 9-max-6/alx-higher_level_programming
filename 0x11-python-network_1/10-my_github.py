d1756ae9518c4396ca786c4950bbb028d7b312dd
9-max-6 <mutukumaxwel@gmail.com>
2024-04-27 09:14:16 +0300

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Script to authenticate Github login
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if len(argv) > 2:
+        username = argv[1]
+        password = argv[2]
+
+        url = "https://github.com/login"
+        try:
+            with requests.get(url, auth=(username, password)) as resp:
+                resp.raise_for_status()
+                print(resp.json().get("id"))
+        except requests.exceptions.Exception as err:
+            pass
