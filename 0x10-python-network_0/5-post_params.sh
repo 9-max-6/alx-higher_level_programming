68cf3a255e1567dd756da57c84c5fbfbd3bd03c3
9-max-6 <mutukumaxwel@gmail.com>
2024-04-28 09:32:04 +0300

    Fix: -s

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST req to the passed URL, and disp the body of the 
-curl -X POST "$1" -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD"
+curl -s -X POST "$1" -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD"
